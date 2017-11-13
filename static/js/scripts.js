e06aa5dd531c321368a4073be6f38013bdc712a6
M.Vainauskas <mindelis1@gmail.com>
2017-11-13 19:12:27 +0000

    Added canvas with free drawing option. Using fbric.js library to achieve desired functionality

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,15 +1,6 @@
 //Setting up image handling to load into canvas
-function handleImage(imgPath){
-    var canvas = document.getElementById('canvas');
-    if (canvas.getContext){
-        var ctx = canvas.getContext('2d');
-        var image = new Image();
-
-        image.onload = function(){
-            ctx.drawImage(image, 0, 0)
-            ctx.fillStyle = "rgba(200, 0, 0, 0.5)";
-            ctx.fillRect(0, 0, 500, 500);
-        };        
-        image.src = imgPath;
-    }
+function drawShape(){
+    var canvas = document.getElementById('numCanvas');
+    var ctx = canvas.getContext('2d');
+    ctx.fillRect(20, 20, 100, 100);
 }
